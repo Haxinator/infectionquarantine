4031e5dd1e9d7cd46f4b1d53f130ff78f8bafb06
Joshua N <joshuarachpaul@hotmail.com>
2025-06-23 18:26:42 +0800

    Looking up/down Mechanic
    Finished basic looking up and down mechanic using ColorRect as placeholders.

---
+++ Scripts/user_inferface.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_go_down_mouse_entered() -> void:
+	get_node("Camera2D").position.y += 648
+
+func _on_go_up_mouse_entered() -> void:
+	get_node("Camera2D").position.y -= 648
