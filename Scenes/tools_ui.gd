801046f67654130dd151e6503682c1e1ad0a35b8
Molflyn <Molflyn@users.noreply.github.com>
2025-06-23 22:15:15 +0800

    Placeholder Desk and Tool UI

---
+++ Scenes/tools_ui.gd
@@ -0,0 +1,26 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_thermometer_tool_placeholder_pressed() -> void:
+	pass # Replace with function body.
+	print("thermometer tool activated!")
+
+
+func _on_stethoscope_tool_placeholder_pressed() -> void:
+	pass # Replace with function body.
+	print("stethoscope tool activated!")
+
+
+func _on_syringe_tool_placeholder_pressed() -> void:
+	pass # Replace with function body.
+	print("syringe tool activated!")
