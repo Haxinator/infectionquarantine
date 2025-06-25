4e582005128ab267a2f977b597ab4abc577b3d74
Joshua N <joshuarachpaul@hotmail.com>
2025-06-25 16:27:30 +0800

    Patient Logic

--- Scripts/patient.gd
+++ Scripts/patient.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal dialogueStart
+
+#Most likely randomise character here.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,3 +11,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	#if mouse button pressed
+	if event is InputEventMouseButton and event.pressed:
+		print("click!")
+		runDiagolue("Patient1")
+
+func runDiagolue(dialogue: String):
+	dialogueStart.emit()
+	Dialogic.start(dialogue)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	print("free")
+	queue_free()
--- Scripts/scene_1.gd
+++ Scripts/scene_1.gd
@@ -3,6 +3,10 @@ extends Node2D
 signal dialogueStart
 
 const nurse = preload("res://Scenes/nurse.tscn")
+const patient = preload("res://Scenes/Patient.tscn")
+
+var seeingPatient = true
+var infront = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,14 +15,22 @@ func _ready() -> void:
 	nurseCpy.position = get_viewport_rect().size/2
 	add_child(nurseCpy)
 	runDiagolue("NurseIntro")
+	infront = nurseCpy
 
 func DialogicSignal(arg: String):
 	if arg == "WalkAway":
-		get_node("Nurse/AnimationPlayer").play("WalkAway")
+		infront.get_node("AnimationPlayer").play("WalkAway")
+		seeingPatient = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not seeingPatient:
+		seeingPatient = true
+		var patientCpy = patient.instantiate()
+		infront = patientCpy
+		patientCpy.position = get_viewport_rect().size/2
+		#patientCpy.get_node("AnimationPlayer").play("WalkIn")
+		add_child(patientCpy)
 
 func runDiagolue(dialogue: String):
 	dialogueStart.emit()
