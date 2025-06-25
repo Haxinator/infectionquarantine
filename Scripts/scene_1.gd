089cbde63074f195c2200d98eb57e74e6ae7f846
Joshua N <joshuarachpaul@hotmail.com>
2025-06-25 13:54:23 +0800

    Nurse and WalkingAway Animation

--- Scripts/scene_1.gd
+++ Scripts/scene_1.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 signal dialogueStart
 
+const nurse = preload("res://Scenes/nurse.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var nurseCpy = nurse.instantiate()
+	Dialogic.signal_event.connect(DialogicSignal)
+	nurseCpy.position = get_viewport_rect().size/2
+	add_child(nurseCpy)
 	runDiagolue("NurseIntro")
 
+func DialogicSignal(arg: String):
+	if arg == "WalkAway":
+		get_node("Nurse/AnimationPlayer").play("WalkAway")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
