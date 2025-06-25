9bc68d01b59bf8d8cb7a62b4c0eb759fd473c40b
Molflyn <143250086+Molflyn@users.noreply.github.com>
2025-06-25 12:56:53 +0800

    Added Clickable Areas System

--- Scripts/user_inferface.gd
+++ Scripts/user_inferface.gd
@@ -15,3 +15,11 @@ func _on_go_down_mouse_entered() -> void:
 
 func _on_go_up_mouse_entered() -> void:
 	get_node("Camera2D").position.y -= 648
+
+
+func _on_microsope_pressed() -> void:
+	print("Microscope Pressed!")
+
+
+func _on_symptoms_folder_pressed() -> void:
+	print("Symptoms Folder Pressed!")
