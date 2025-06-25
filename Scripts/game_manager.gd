c49a5a5577ff8a5ef5f78e8ea741e32782964c80
Molflyn <143250086+Molflyn@users.noreply.github.com>
2025-06-25 15:39:29 +0800

    Added Game Manager Level Recognition
    - The game manager now recognises the current level based on its filename
    - Also added scene_2 and scene_3 as extra levels/scenes

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,12 +3,14 @@ extends Node2D
 ## Sets the number of patients to get through that day/level
 @export var number_of_patients : int = 0
 
-## Gets current level (finds the first child node of the root) - IN PROGRESS
-
+## Gets current level (follows the "x_1" level naming convention)
+@onready var current_level_node : String = get_tree().get_first_node_in_group("Level").scene_file_path
+@onready var current_level : int = int(current_level_node.get_slice("_", 1))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	if current_level:
+		print("level: " + str(current_level))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
