428ebb8299ea1ff1c91b1b417adf6645b5977f90
Molflyn <Molflyn@users.noreply.github.com>
2025-06-30 11:20:35 +0800

    Start button now loads scene_1

--- Scripts/start_screen.gd
+++ Scripts/start_screen.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Managers/GameManager.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/scene_1.tscn")
